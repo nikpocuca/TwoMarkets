a38eaab1c3dd7520c3f3d9b59285f35a9369cf38
Nik Pocuca <pocucan@Niks-MacBook-Air.local>
2018-03-01 18:59:15 -0500

    added in data requests for TWMJF

---
+++ twoMarkets.R
@@ -0,0 +1,8 @@
+library(quantmod)
+getSymbols("TWMJF",source ="google")
+plot(TWMJF)
+newTWM <- TWMJF
+colnames(TWMJF) <- colnames(TWMJF)
+colnames(newTWM) <- substring(text = colnames(TWMJF), first = 7)
+times <- index(newTWM)
+getSymbols("WEED.TO",source = "google")
