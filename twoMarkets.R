67c9af398927b639f3e0085b0019813b9ccd2fd3
Nik Pocuca <pocucan@Niks-MacBook-Air.local>
2018-03-08 17:47:05 -0500

    abx found

--- twoMarkets.R
+++ twoMarkets.R
@@ -6,3 +6,9 @@ colnames(TWMJF) <- colnames(TWMJF)
 colnames(newTWM) <- substring(text = colnames(TWMJF), first = 7)
 times <- index(newTWM)
 getSymbols("WEED.TO",source = "google")
+
+
+
+
+getSymbols("ABX",source = "google")
+getSymbols("ABX.TO",source = "yahoo")
