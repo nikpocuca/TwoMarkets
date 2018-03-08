ae2430878284069d5d4dc777bdfcc8771402a65e
Nik Pocuca <pocucan@Niks-MacBook-Air.local>
2018-03-08 18:25:28 -0500

    Function added for parsing

--- twoMarkets.R
+++ twoMarkets.R
@@ -1,5 +1,15 @@
 library(quantmod)
-getSymbols("TWMJF",source ="google")
+
+getAndParse <- function(string,src){
+  getSymbols(string,source = src)
+  newStock <- get(string)
+  colnames(newStock) <- substring(text = colnames(get(string)), first = (nchar(string) +2))
+  return(as.data.frame(newStock))
+}
+# use getAndParse from now on instead of getSymbols
+newTWM <- getAndParse("TWMJF","google")
+
+
 plot(TWMJF)
 newTWM <- TWMJF
 colnames(TWMJF) <- colnames(TWMJF)
@@ -8,7 +18,10 @@ times <- index(newTWM)
 getSymbols("WEED.TO",source = "google")
 
 
-
+canadaDay <- "2017-07-03"  # canadian side closed, american open.
+civicHoliday <- "2015-08-03" #canadian closed, american open. 
 
 getSymbols("ABX",source = "google")
 getSymbols("ABX.TO",source = "yahoo")
+
+
