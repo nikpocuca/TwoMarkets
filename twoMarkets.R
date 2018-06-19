d4ec13ff5120e8a98ce1edca3cb9a1a3f43c24e0
Nik Pocuca <pocucan@Niks-MacBook-Air.local>
2018-06-18 21:07:19 -0400

    Data api structure is now complete, now to write a function that extracts the data near the holiday dates.

--- twoMarkets.R
+++ twoMarkets.R
@@ -29,16 +29,11 @@ checkDOW(sampleDates,civic)
 
 # next function to code is one that extracts previous day date. 
 
-
-
 plot(TWMJF)
 newTWM <- TWMJF
 colnames(TWMJF) <- colnames(TWMJF)
 colnames(newTWM) <- substring(text = colnames(TWMJF), first = 7)
 times <- index(newTWM)
-
-
-
 
 getSymbols("WEED.TO",source = "google")
 
@@ -48,8 +43,60 @@ civicHoliday <- "2015-08-03" #canadian closed, american open.
 getSymbols("ABX",source = "google")
 getSymbols("ABX.TO",source = "yahoo")
 
-
-
 as.POSIXlt("2018-03-17")$wday
+
+#American Dates 
+civicHoliday <- "2015-08-03" 
+americanStringDates <- c("2015-08-03")
+
+#Canadian Dates  
+canadaDay <- "2017-07-03"  # canadian side closed, american open.
+canadianStringDates <- c("2017-07-03")
+
+#Stock Strings
+stkString <- c("CanopyGrowth,WEED.TO,TWMJF","Berrick Gold Group,ABX.TO,ABX" ) 
+
+# MasterList Function 
+createMasterList <- function(canadianDates,americanDates,StockStrings) {
+  # Generate Stock Name and Ticker Info List.
+  #need to figure this part out 
+  TickerListInfo <- parseStkString(stockStrings = StockStrings)
+  MasterDataList <- list(tickerMap = TickerListInfo)
+  for (i in 1:length(TickerListInfo$CmpnyName)) {
+  MasterDataList[[TickerListInfo[i,1]]] <- genStockObj(TickerListInfo[i,1],TickerListInfo[i,2],TickerListInfo[i,3])
+  }
+  # now to add in the function that extracts relevant data points. 
+  
+  return(MasterDataList)
+}
+
+#Parsing stock string 
+parseStkString <- function(stockStrings) {
+  StkStringFrame <- data.frame(1,2,3)
+  colnames(StkStringFrame) <- c("CmpnyName","CanadianTicker","AmericanTicker")
+  for (stockString in stockStrings) {
+  unlistedString <- unlist(strsplit(stockString,","))
+  StkStringFrame <- rbind(StkStringFrame,unlistedString)
+  }
+  StkStringFrame <- StkStringFrame[-1,]
+  return(StkStringFrame)
+}
+
+#Generate list object for stock 
+genStockObj <- function(cName,cStkTicker,aStkTicker){
+  return(list(
+    Company = cName,
+    CanData = getAndParse(cStkTicker,"google"),
+    AmerData = getAndParse(aStkTicker,"google")
+  ))
+}
+
+#genTest 
+genStockObj("CanopyGrowth","WEED.TO","TWMJF")
+
+test <- createMasterList(canadianDates = canadianStringDates,
+                 americanDates = americanStringDates,
+                 StockStrings = stkString)
+
 
 
